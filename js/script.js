e04741accf183c594c40da2d9a3df4cf1f86c49a
AntonioFBove <antoniofbove@gmail.com>
2022-01-17 17:07:08 +0100

    milestone 2

--- js/script.js
+++ js/script.js
@@ -6,7 +6,19 @@ new Vue({
         text: "fare la spesa",
         done: false,
       },
+      {
+        text: "fare icompiti per boolean",
+        done: false,
+      },
+      {
+        text: "fare la nanna presto",
+        done: false,
+      },
     ],
   },
-  methods: {},
+  methods: {
+    closeItem: function(index) {
+      this.todo.splice(index, 1);
+    },
+  },
 });
